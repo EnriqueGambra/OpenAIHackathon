ae76cc8d9a2cc2357b9469eeab1c65dc6d22c7d0
Enrique Gambra <egambra@ntst.com>
2023-06-16 14:29:23 -0400

    pushing changes.

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,5 +1,5 @@
 import { Subject, tap } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Message } from './conversation-window/conversation-window.component';
 
@@ -7,7 +7,10 @@ import { Message } from './conversation-window/conversation-window.component';
   providedIn: 'root',
 })
 export class MessageService {
-  endpoint: string = 'www.google.com';
+  private endpoint: string = 'https://text-to-embedding.azurewebsites.net/api/HttpTrigger1?';
+
+  private xFunctionKey: string = '-MAJtoShM1cFbDjW2vPrEU3Ehxx6Sq1mxtzuskCnxX5rAzFuy77kBg==';
+
   messageReceived: Subject<Message> = new Subject();
 
   constructor(private HttpClient: HttpClient) {}
@@ -26,18 +29,31 @@ export class MessageService {
       sender: 'Bot'
     }
 
-    setTimeout(() => {
-      this.messageReceived.next(botResponse);
-    }, 500);
+    const payload = {
+      input: message,
+    }
 
-    
+    const headers = new HttpHeaders({
+      // 'Content-Type': 'application/json',
+      'x-function-keys': this.xFunctionKey,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'DELETE, POST, GET',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+    });
 
-    // this.HttpClient.post(this.endpoint, message).pipe(
-    //   tap((event: any) => {
-    //     if (event.ok) {
-    //       this.messageReceived.next(event)
-    //     }
-    //   })
-    // );
+    const options = { headers: headers };
+
+    // setTimeout(() => {
+    //   this.messageReceived.next(botResponse);
+    // }, 500);
+
+    this.HttpClient.post(this.endpoint, payload, options).pipe(
+      tap((event: any) => {
+        console.log("EVENT RESPONSE IS: ", event);
+        if (event.ok) {
+          this.messageReceived.next(event)
+        }
+      })
+    ).subscribe();
   }
 }
